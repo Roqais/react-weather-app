42ee4a8217f014f6b1358697a140ce34ea6bac83
Roqais Mahmood <roqaiskhan121@gmail.com>
2024-08-02 23:21:07 +0500

    changes

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -8,6 +8,8 @@ import wind from "../Assets/wind.png";
 import smoke from "../Assets/smoke.png";
 import { useEffect, useState, useRef } from "react";
 
+import back from '../Assets/back.png'
+
 
 
 
@@ -99,7 +101,10 @@ const Weather = () => {
 
     return (
         <>
-            <div className="min-h-screen grid grid-cols-5 md:bg-cover md:bg-center md:bg-[url('src/Assets/back.png')] ">
+            <div
+                className="min-h-screen grid grid-cols-5 md:bg-cover md:bg-center"
+                style={{ backgroundImage: `url(${back})` }}
+            >
                 <div className="col-span-5">
                     <div className="max-w-md mx-auto min-h-screen md:min-h-0 md:my-6 md:py-20 pt-10  text-white md:px-10 px-8 bg-cyan-900 md:bg-cyan-900 md:bg-opacity-70 rounded-none md:rounded-3xl md:shadow-2xl border-none md:border-transparent">
                         <form>
